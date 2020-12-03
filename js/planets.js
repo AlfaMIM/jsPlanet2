fee4f5e60d468fed4b0f3de8a83c69bc97226dbc
Opilane TTHK <opilane@tthk.ee>
2020-12-03 09:45:33 +0200

    commit

--- js/planets.js
+++ js/planets.js
@@ -1,20 +1,10 @@
 let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
 planets.push(`Pluto`)
-console.log(planets);
+console.log(planets.length);
+let output = ``;
 
-const html = `
-    <ul>
-        <li>${planets[0]}</li>
-        <li>${planets[1]}</li>
-        <li>${planets[2]}</li>
-        <li>${planets[3]}</li>
-        <li>${planets[4]}</li>
-        <li>${planets[5]}</li>
-        <li>${planets[6]}</li>
-        <li>${planets[7]}</li>
-        <li>${planets[8]}</li>
-    
-    
-    </ul>
-    `;
-    document.body.innerHTML = html;
+for(let i = 0; i < planets.length; i++){
+    console.log(i+1, planets[i]);
+    output += `<li>${planets[i]}</li>`;
+}
+    document.querySelector(".list").innerHTML = output
